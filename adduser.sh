54a49f47a593bf37209d774be69b0c2a097243f1
stevenservo <saspring1144@gmail.com>
2022-01-28 21:06:50 -0800

    Add ability to give user sudo access with prompt

--- adduser.sh
+++ adduser.sh
@@ -10,9 +10,10 @@ add_user()
 	read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 	echo "Adding the user $USER.."
 	sudo useradd -c "$COMMENTS" $USER
-	echo passwd --stdin $USER $PASSWORD
+	echo -e "$PASSWORD\n$PASSWORD" | sudo passwd $USER
 	echo "Added user $USER with password $PASSWORD"
-
+	read -p "Would you like to give the user SUDO access? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+	sudo usermod -a -G sudo $USER
 }
 echo "script started.."
 add_user
