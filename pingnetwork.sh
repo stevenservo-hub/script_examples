27a6b76bf127508cb092ae484227d6da19729528
stevenservo <saspring1144@gmail.com>
2022-04-06 17:32:54 -0700

    added nested loop.

--- pingnetwork.sh
+++ pingnetwork.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/bash
 
 ifconfig | grep "broadcast" | cut -d " " -f 10 | cut -d "." -f 1,2,3 | uniq > octets.txt
-
-OCTETS=$(cat octets.txt)
-
-echo "" > $OCTETS.txt
-
-for ip in {1..254}
+echo "looking for active devices on the network"
+cat octets.txt | while read line || [[ -n $line ]];
 do
-       ping -c 1 $OCTETS.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> $OCTETS.txt &
+	for ip in {1..254}
+	do
+	ping -c 1 $line.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> octets.txt &
+	done
 done
 
-cat $OCTETS.txt | sort > sorted_$OCTETS.txt
-
-nmap -sS 20 -iL sorted_$OCTETS.txt
+sudo nmap -sS 20 -iL octets.txt
 exit
+
