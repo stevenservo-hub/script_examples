d069c8ccdf971f104294ca291c4f0acfb472730d
stevenservo <saspring1144@gmail.com>
2022-08-04 12:42:36 -0700

    fixed errors preventing script to run

--- bash_scripts/autocx.sh
+++ bash_scripts/autocx.sh
@@ -10,7 +10,7 @@ set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+trap 'echo "\"${last_command}\"command returned exit code $?."' EXIT
 
 #This function checks the current user to ensure root
 root_check() {
@@ -22,64 +22,16 @@ fi
 
 # Print usage statement, to guide users
 print_usage() {
-printf "Usage: `basename` -p <number> -l <lastname> \n you will be prompted 
-to add a password, this is the clients sip secret. " #Todo, add usage 
-}
-
-# Add flags needed to build sip config
-options() {
-TEMP=`getopt --long -o "p:l:" "$@"`
-eval set -- "$TEMP"
-while true ; do
-    case "$1" in
-
-        -p )
-	    phone=$2
-            shift 2
-        ;;
-        -l )
-	    last=$2
-            shift 2
-        ;;
-        *)
-          print_usage # This can be handled better
-        ;;
-    esac 
-done;
-if [ $(( $# - $OPTIND )) -lt 1 ];then
-    echo "Usage: `basename` -p <number> -l <lastname>"
-    exit 1
-fi
-# Take in client secret adding some fancy bits and the ability to backspace
-password=''
-echo "please enter sip users secret:\n(this is used to authenticate user to the sip server)"
-while IFS= read -r -s -n1 char; do
-  [[ -z $char ]] && { printf '\n'; break; } # ENTER pressed; output \n and break.
-  if [[ $char == $'\x7f' ]]; then # backspace was pressed
-      # Remove last char from output variable.
-      [[ -n $password ]] && password=${password%?}
-      # Erase '*' to the left.
-      printf '\b \b' 
-  else
-    # Add typed char to output variable.
-    password+=$char
-    # Print '*' in its stead.
-    printf '*'
-  fi
-done
+printf "Usage: autocx -phone '5555555555' -last 'lastname' " #Todo, add usage 
 }
 
 # build file system just a rough draft need to add a more elegant apr oach
 mk_files() {
-sip_dir=/etc/asterisk/customer/"$last"
+sip_dir=/etc/asterisk/customers/"$last"
 root_check	
 mkdir "$sip_dir"
 touch "$sip_dir"/sip.conf && touch "$sip_dir"/extensions.conf 
-chown -r asterisk:asterisk "$sip_dir"
-if [ ! -f "$sip_dir" ]; then
-	echo "error creating directory: $sip_dir does not exist"
-	exit 1
-fi
+chown -R asterisk:asterisk "$sip_dir"
 }
 # Building the config files  	
 build_config() {
@@ -109,11 +61,50 @@ build_config() {
 		echo "same => n,Dial(SIP/65057962*\${EXTEN}@flowroute)"
 		echo "same => n,Hangup()"
 	} >> "$sip_dir"/extensions.conf
-echo "#include customers/$last/extensions.conf" >> "$sip_dir"/extensions.conf
-echo "#include customers/$last/sip.conf" >> "$sip_dir"/sip.conf
+echo "#include customers/$last/extensions.conf" >> /etc/asterisk/customers/extensions.conf
+echo "#include customers/$last/sip.conf" >> etc/asterisk/customers/sip.conf
 }
 
-options
+while test $# -gt 0; do
+           case "$1" in
+		-phone)
+                    shift
+		    phone=$1
+                    shift
+                    ;;
+                -last)
+                    shift
+                    last=$1
+                    shift
+                    ;;
+                -h)
+		    print_usage
+		    exit 1;
+                    ;;
+                *)
+                   echo "$1 is not a recognized flag!"
+		   print_usage
+                   exit 1;
+                   ;;
+          esac
+  done  
+password=''
+echo "please enter sip users secret:"
+while IFS= read -r -s -n1 char; do
+  [[ -z $char ]] && { printf '\n'; break; } # ENTER pressed; output \n and break.
+  if [[ $char == $'\x7f' ]]; then # backspace was pressed
+      # Remove last char from output variable.
+      [[ -n $password ]] && password=${password%?}
+      # Erase '*' to the left.
+      printf '\b \b' 
+  else
+    # Add typed char to output variable.
+    password+=$char
+    # Print '*' in its stead.
+    printf '*'
+  fi
+done
+
 mk_files
 build_config
 
